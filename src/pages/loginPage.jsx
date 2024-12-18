83aabba150ced7a34b6084b77d787c35d3510b25
Lukmaan Kamiz <m.lukmaankamiz@gmail.com>
2024-12-18 09:55:41 +0530

    Created CSS for Login Page and Linked with Login Page➕🔗

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,10 +1,12 @@
-export function LoginPage(){
-    return(
+import './LoginPage.css';
+
+export function LoginPage() {
+    return (
         <div>
-        <h1>Login</h1>
-        <input type="text" placeholder="Enter Your User Name" />
-        <input type="password" placeholder="Enter Your Password" />
-        <button>Login To Your Accoutn</button>
+            <h1>Login</h1>
+            <input type="text" placeholder="Enter Your User Name" />
+            <input type="password" placeholder="Enter Your Password" />
+            <button>Login To Your Account</button>
         </div>
-    )
+    );
 }
