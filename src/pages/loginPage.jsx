bf723dae8a68468455b80bb51f03d4e74aec22e7
Lukmaan Kamiz <m.lukmaankamiz@gmail.com>
2024-12-18 09:47:15 +0530

    Created Login Page➕

---
+++ src/pages/loginPage.jsx
@@ -0,0 +1,10 @@
+export function LoginPage(){
+    return(
+        <div>
+        <h1>Login</h1>
+        <input type="text" placeholder="Enter Your User Name" />
+        <input type="password" placeholder="Enter Your Password" />
+        <button>Login To Your Accoutn</button>
+        </div>
+    )
+}
