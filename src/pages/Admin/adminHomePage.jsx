12e42569f0e89ae85aea7d4ffba3312db3713c7b
Lukmaan Kamiz <m.lukmaankamiz@gmail.com>
2024-12-19 11:56:14 +0530

    Add React Icons Library and Use in Admin Side Bar➕⬆️

--- src/pages/Admin/adminHomePage.jsx
+++ src/pages/Admin/adminHomePage.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
+import { TbLayoutDashboardFilled } from "react-icons/tb";
+import { FaShoppingBag } from "react-icons/fa";
+import { GoListOrdered } from "react-icons/go";
+import { HiIdentification } from "react-icons/hi2";
 export function AdminHomePage() {
     return(
         <div className="bg-gray-400 w-full h-screen flex">
             <div className="bg-blue-100 w-[25%] h-screen flex flex-col items-center">
-            <Link to="/admin/dahboard">Dashboard</Link>
-            <Link to="/admin/products">Products</Link>
-            <Link to="/admin/orders">Orders</Link>
-            <Link to="/admin/Customers">Customers</Link>
+            <Link className="flex flex-row items-center" to="/admin/dahboard"><TbLayoutDashboardFilled />Dashboard</Link>
+            <Link className="flex flex-row items-center" to="/admin/products"><FaShoppingBag />Products</Link>
+            <Link className="flex flex-row items-center" to="/admin/orders"><GoListOrdered />Orders</Link>
+            <Link className="flex flex-row items-center" to="/admin/Customers"><HiIdentification />Customers</Link>
             </div>
 
             <div className="w-[75%] h-screen">
