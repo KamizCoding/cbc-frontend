fcef7d31ec684c95b62d767ef06cacdb17b0e044
Lukmaan Kamiz <m.lukmaankamiz@gmail.com>
2024-12-19 11:39:06 +0530

    Set Up the Admin SideBar Links🔗

--- src/pages/Admin/adminHomePage.jsx
+++ src/pages/Admin/adminHomePage.jsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
 export function AdminHomePage() {
     return(
-        <div>
-            Admin Home Page
+        <div className="bg-gray-400 w-full h-screen flex">
+            <div className="bg-blue-100 w-[25%] h-screen flex flex-col items-center">
+            <Link to="/admin/dahboard">Dashboard</Link>
+            <Link to="/admin/products">Products</Link>
+            <Link to="/admin/orders">Orders</Link>
+            <Link to="/admin/Customers">Customers</Link>
+            </div>
+
+            <div className="w-[75%] h-screen">
+
+            </div>
         </div>
     );
 }
