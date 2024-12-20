ca589c15e65c1ff404018990a0276b3b6a208d88
Lukmaan Kamiz <m.lukmaankamiz@gmail.com>
2024-12-20 19:42:41 +0530

    Integrate with Back End with AXIOS.🔗

--- src/pages/Admin/adminProductsPage.jsx
+++ src/pages/Admin/adminProductsPage.jsx
@@ -1,7 +1,17 @@
+import axios from "axios";
+
 export function AdminProductsPage() {
+
+    getProducts()
+    
     return(
         <div>
             Admin Products Page
         </div>
     );
+}
+
+async function getProducts(){
+    const res = await axios.get("http://localhost:5000/api/products")
+        console.log(res)
 }
