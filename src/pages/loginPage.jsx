e2b79026029b2ca19e738e0bfd4fc5682bb02271
Lukmaan Kamiz <m.lukmaankamiz@gmail.com>
2024-12-24 11:53:59 +0530

    Enhanced the Login Page and Replaced Logo⬆️🔄️

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,13 +1,13 @@
 export default function LoginPage() {
     return (
-        <div className="flex flex-col min-h-screen items-center justify-center bg-gray-100">
-            <img src="/logo.png" className="rounded-full w-[150px] p-3" />
-            <div className="w-full max-w-md bg-white rounded-lg shadow-md p-6">
-                <h1 className="text-2xl font-bold text-center text-gray-800 mb-6">Login</h1>
+        <div className="flex flex-col min-h-screen items-center justify-center bg-gradient-to-b from-green-300 to-green-500">
+            <img src="/logo1.png" className="rounded-full w-[150px] p-3" />
+            <div className="w-full max-w-md bg-gradient-to-br from-green-500 to-green-700 rounded-lg shadow-md p-6">
+                <h1 className="text-2xl font-bold text-center text-gray-100 mb-6">Login</h1>
                 <div className="space-y-4">
                     <input
                         type="text"
-                        placeholder="Enter Your User Name"
+                        placeholder="Enter Your Email"
                         className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                     />
                     <input
