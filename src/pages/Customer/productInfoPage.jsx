1762c91806f7e7f8982c89a9c033a577536639dc
Lukmaan Kamiz <m.lukmaankamiz@gmail.com>
2025-02-06 21:14:14 +0530

    Set Up Prdouct Found Status with Product Details➕🖼️

--- src/pages/Customer/productInfoPage.jsx
+++ src/pages/Customer/productInfoPage.jsx
@@ -19,24 +19,35 @@ export default function ProductInfoPage() {
         if (res.data == null) {
           setStatus("not-found");
         }
+
+        //If Product Was Found
+        if(res.data != null){
+            setProduct(res.data)
+            setStatus("found")
+        }
       });
   }, []);
 
   return (
     <div>
       {status == "loading" && (
-        <div className="fixed top-0 left-0 w-full h-screen flex flex-col items-center justify-center bg-opacity-50 bg-secondary">
+        <div className="fixed top-0 left-0 w-full h-screen flex flex-col items-center justify-center bg-opacity-50 bg-primary">
           <div className="w-32 h-32 border-t-2 border-b-2 border-gray-300 border-t-dark border-b-dark rounded-full animate-spin"></div>
           <p className="mt-4 text-dark text-xl font-semibold animate-pulse">
             Loading...
           </p>
         </div>
       )}
-      {status == "not-found" && (
-        <div className="flex flex-col items-center justify-center h-screen text-center">
-          <h1 className="text-4xl font-bold text-accent">Product Not Found</h1>
-          <p className="text-gray-700 text-lg mt-2">
-            The product you are looking for does not exist or has been removed.
+      {
+      status == "not-found" && (
+        <div className="flex flex-col items-center justify-center h-screen text-center bg-primary">
+          <h1 className="text-6xl font-extrabold text-accent">404</h1>
+          <h2 className="text-3xl font-bold text-gray-800 mt-2">
+            Oops! This Product is Missing
+          </h2>
+          <p className="text-gray-600 text-lg mt-2">
+            We couldn’t find the product you were looking for. It may have been
+            removed or never existed.
           </p>
           <Link
             to="/products"
@@ -47,7 +58,22 @@ export default function ProductInfoPage() {
         </div>
       )}
 
-      {status == "found" && <div>product found</div>}
+      {status == "found" && (
+        <div className="w-full h-full flex items-center justify-center">
+            <div className="w-[35%] h-full">
+                <img src={product.images[0]} className="w-full h-[300px] object-cover rounded-lg" />
+            </div>
+            <div className="w=[65%] h-full p-4">
+                <h1 className="text-4xl font-bold text-gray-800">{product.productName}</h1>
+                <h1 className="text-2xl font-semibold text-gray-500">{product.altNames.join(" | ")}</h1>
+                <p className="text-xl text-gray-600">{(product.price>product.lastPrice)&&
+                    <span className="line-through text-red-500">${product.price}</span>}
+                    <span>${product.lastPrice}</span>
+                </p>
+                <p className="text-xl text-gray-600">{product.description}</p>
+            </div>
+        </div>
+      )}
     </div>
   );
 }
