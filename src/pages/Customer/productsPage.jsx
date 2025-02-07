1ea6009e1b7e8d5b0d9ce8e845eda7cfee7f9bc0
Lukmaan Kamiz <m.lukmaankamiz@gmail.com>
2025-02-07 17:59:49 +0530

    Fetch Product Details From Database For Products Page🫴

--- src/pages/Customer/productsPage.jsx
+++ src/pages/Customer/productsPage.jsx
@@ -1,7 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
 export default function ProductsPage() {
-    return(
-        <div>
-            Customer Products Page
-        </div>
-    );
+  const [products, setProducts] = useState([]);
+  const [loadingStatus, setLoadingStatus] = useState("loading");
+
+  useEffect(() => {
+    if (loadingStatus === "loading") {
+      axios
+        .get(import.meta.env.VITE_BACKEND_URL + "/api/products")
+        .then(
+            (res) => {
+                console.log(res.data)
+                setProducts(res.data)
+                setLoadingStatus('loaded')
+            }
+        ).catch(
+            (err)=>toast.error("Failed To Fetch Products"))
+    }
+  },[]);
+  return (
+    <div>
+        Customer Products Page
+    </div>)
 }
