fa72229d2280d1945023d1867e91a937ec2376c6
Lukmaan Kamiz <m.lukmaankamiz@gmail.com>
2025-02-11 21:23:16 +0530

    Implemented Functionaly to Fetch Info of Products in Cart🛒🧴

--- src/components/cartCard.jsx
+++ src/components/cartCard.jsx
@@ -1,7 +1,30 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 export default function CartCard(props){
 
-    const productId = props.productId
+    const productId = props.productId 
     const quantity = props.quantity
+
+    const [product, setProduct] = useState(null)
+    const [loaded, setLoaded] = useState(false)
+
+    useEffect(() => {
+        if(!loaded){
+            axios.get(import.meta.env.VITE_BACKEND_URL + "/api/products/" + productId).then(
+                (response) => {
+                    setProduct(response.data)
+                    console.log(response.data)
+                    setLoaded(true)
+                }
+            ).catch(
+                (error) => {
+                    console.log(error)
+                }
+            )
+        }
+    })
+
     return(
         <div className="border w-1/2 flex justify-between items-center">
             <span>{productId}</span>
