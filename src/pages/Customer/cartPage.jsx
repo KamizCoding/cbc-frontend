1eeb105f348597c6b7bfdcbbe69bc8c062540987
Lukmaan Kamiz <m.lukmaankamiz@gmail.com>
2025-02-15 20:18:50 +0530

    Implement Null Check For Cart0️⃣🛒

--- src/pages/Customer/cartPage.jsx
+++ src/pages/Customer/cartPage.jsx
@@ -16,8 +16,10 @@ export default function CartPage() {
             orderedItems: loadCart()
         }).then((res) => {
             console.log(res.data);
+            if(res.data == null){
             setTotal(res.data.total);
             setLabeledTotal(res.data.labeledTotal);
+            }
         });
     }, []);
 
