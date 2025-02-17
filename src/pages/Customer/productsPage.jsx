81114fe901a25763afde3cf107531899aa4fccb4
Lukmaan Kamiz <m.lukmaankamiz@gmail.com>
2025-02-17 19:11:40 +0530

    Created The Search Bar in Products Page🔍🎂

--- src/pages/Customer/productsPage.jsx
+++ src/pages/Customer/productsPage.jsx
@@ -9,23 +9,30 @@ export default function ProductsPage() {
 
   useEffect(() => {
     if (loadingStatus === "loading") {
-      axios.get(import.meta.env.VITE_BACKEND_URL + "/api/products")
+      axios
+        .get(import.meta.env.VITE_BACKEND_URL + "/api/products")
         .then((res) => {
-                console.log(res.data)
-                setProducts(res.data.list)
-                setLoadingStatus('loaded')
-            }
-        ).catch(
-            (err)=>toast.error("Failed To Fetch Products"))
+          setProducts(res.data.list);
+          setLoadingStatus("loaded");
+        })
+        .catch((err) => toast.error("Failed To Fetch Products"));
     }
-  },[]);
-  
+  }, []);
+
   return (
-    <div className="w-full flex flex-wrap justify-center gap-4 overflow-y-auto h-[calc(100vh-150px)] p-4">
-        {
-            products.map((product, index) => (
-                <ProductCard key={product.id || index} product={product} />
-              ))
-        }
-    </div>)
+    <div className="w-full flex flex-col items-center gap-4 p-4">
+      <div className="w-full flex justify-center mb-6">
+        <input
+          type="text"
+          className="w-1/2 p-3 rounded-lg border border-gray-300 shadow-md focus:outline-none focus:ring-2 focus:ring-green-500"
+          placeholder="Search Products"
+        />
+      </div>
+      <div className="w-full flex flex-wrap justify-center gap-4 overflow-y-auto">
+        {products.map((product, index) => (
+          <ProductCard key={product.id || index} product={product} />
+        ))}
+      </div>
+    </div>
+  );
 }
