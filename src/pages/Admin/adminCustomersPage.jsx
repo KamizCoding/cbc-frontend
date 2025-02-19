1952e0901cd2e6027918131b71f2b3e923c31298
Lukmaan Kamiz <m.lukmaankamiz@gmail.com>
2025-02-19 17:36:18 +0530

    Added Blobked and Delete Buttons🔼➕

--- src/pages/Admin/adminCustomersPage.jsx
+++ src/pages/Admin/adminCustomersPage.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { FaBan, FaTrash } from "react-icons/fa6";
 
 export default function AdminCustomersPage() {
   const [customers, setCustomers] = useState([]);
@@ -28,12 +29,14 @@ export default function AdminCustomersPage() {
           <table className="w-full bg-white rounded-lg border border-gray-300 shadow-md">
             <thead>
               <tr className="bg-lime-700 text-white uppercase text-md font-semibold">
-                <th className="py-4 px-6 text-left">First Name</th>
-                <th className="py-4 px-6 text-left">Last Name</th>
-                <th className="py-4 px-6 text-left">Email</th>
-                <th className="py-4 px-6 text-left">Blocked?</th>
-                <th className="py-4 px-6 text-left">Type</th>
-                <th className="py-4 px-6 text-left">Profile Picture</th>
+                <th className="py-3 px-3 text-left w-20">First Name</th>
+                <th className="py-3 px-3 text-left w-20">Last Name</th>
+                <th className="py-3 px-3 text-left w-40">Email</th>
+                <th className="py-3 px-3 text-left w-16">Blocked?</th>
+                <th className="py-3 px-3 text-left w-16">Type</th>
+                <th className="py-3 px-3 text-left w-28">Profile</th>
+                <th className="py-3 px-3 text-left w-16">Block</th>
+                <th className="py-3 px-3 text-left w-16">Delete</th>
               </tr>
             </thead>
             <tbody className="text-gray-800 text-sm font-medium divide-y divide-gray-200">
@@ -44,21 +47,33 @@ export default function AdminCustomersPage() {
                     index % 2 === 0 ? "bg-lime-100" : "bg-lime-200"
                   } hover:bg-lime-300 transition duration-200`}
                 >
-                  <td className="py-4 px-6 text-left whitespace-nowrap">
+                  <td className="py-3 px-3 text-left whitespace-nowrap">
                     {customer.firstName}
                   </td>
-                  <td className="py-4 px-6 text-left">{customer.lastName}</td>
-                  <td className="py-4 px-6 text-left">{customer.email}</td>
-                  <td className="py-4 px-6 text-left">
+                  <td className="py-3 px-3 text-left">{customer.lastName}</td>
+                  <td className="py-3 px-3 text-left truncate">
+                    {customer.email}
+                  </td>
+                  <td className="py-3 px-3 text-left">
                     {customer.isBlocked ? "Yes" : "No"}
                   </td>
-                  <td className="py-4 px-6 text-left">{customer.type}</td>
-                  <td className="py-4 px-6 text-left">
+                  <td className="py-3 px-3 text-left">{customer.type}</td>
+                  <td className="py-3 px-3 text-left">
                     <img
                       src={customer.profilePicture}
                       alt="Profile"
-                      className="w-12 h-12 rounded-full shadow-md border border-gray-300"
+                      className="w-8 h-8 rounded-full shadow-md border border-gray-300"
                     />
+                  </td>
+                  <td className="py-3 px-3 text-left">
+                    <button className="p-1 rounded-lg bg-yellow-500 hover:bg-yellow-600 text-white shadow-md transition duration-200">
+                      <FaBan />
+                    </button>
+                  </td>
+                  <td className="py-3 px-3 text-left">
+                    <button className="p-1 rounded-lg bg-red-500 hover:bg-red-600 text-white shadow-md transition duration-200">
+                      <FaTrash />
+                    </button>
                   </td>
                 </tr>
               ))}
