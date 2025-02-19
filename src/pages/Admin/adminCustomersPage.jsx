b41ecda383d77db8c8d321f52d8196ea1a5f1829
Lukmaan Kamiz <m.lukmaankamiz@gmail.com>
2025-02-19 16:51:46 +0530

    Fetch Customer Details From Back End to Admin Dash🧑‍🦲📃

--- src/pages/Admin/adminCustomersPage.jsx
+++ src/pages/Admin/adminCustomersPage.jsx
@@ -1,7 +1,31 @@
+import axios from "axios";
+import { useState } from "react";
+
 export default function AdminCustomersPage() {
-    return(
-        <div>
-            Admin Customers Page
-        </div>
-    );
+  const [customers, setCustomers] = useState([]);
+
+  axios.get(import.meta.env.VITE_BACKEND_URL + "/api/users", {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}` 
+    }
+  })
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((err) => console.error(err));
+  
+
+  return (
+    <div>
+      Admin Customers Page
+      {customers.map((customer,index) => {
+        return(
+            <div key={index}>
+                {customer.firstName}
+            </div>
+        )
+      }
+      )}
+    </div>
+  );
 }
