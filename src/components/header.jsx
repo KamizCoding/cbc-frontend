e763387ec36c429f08000743dc9ad03c9d6a52c8
Lukmaan Kamiz <m.lukmaankamiz@gmail.com>
2025-02-26 22:37:08 +0530

    MAde Minor Changes to Enhance Look of The Header⬆️✅

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -131,7 +131,7 @@ export default function Header() {
               </button>
               <button
                 onClick={() => setIsModalOpen(true)}
-                className="px-5 py-2 bg-red-500 text-white font-semibold rounded-lg hover:bg-red-600 transition-all duration-300 shadow-md hover:shadow-xl mb-1"
+                className="px-5 py-1 bg-red-500 text-white font-semibold rounded-lg hover:bg-red-600 transition-all duration-300 shadow-md hover:shadow-xl mb-1"
               >
                 Logout
               </button>
